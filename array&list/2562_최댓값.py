41071bbeed128e8994b1e650d7c6b041480ecf75
appypaw <siment3187@gmail.com>
2023-08-10 23:01:47 +0900

    2562 added

---
+++ array&list/2562_최댓값.py
@@ -0,0 +1,24 @@
+"""
+
+문제
+
+9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
+
+예를 들어, 서로 다른 9개의 자연수
+
+3, 29, 38, 12, 57, 74, 40, 85, 61
+
+이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
+
+"""
+
+numArr = map(int, input().split())
+
+print(max(numArr))
+ind = max(numArr) 
+
+while ind != max(numArr):
+    if int(ind) == int(max(numArr)):
+        break
+
+print
