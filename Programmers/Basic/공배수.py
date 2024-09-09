95afca9459eabe74bce39b88f749762ba00db44e
appypaw <siment3187@gmail.com>
2024-09-09 17:56:42 +0900

    Programmers 공배수.py solved

--- Programmers/Basic/공배수.py
+++ Programmers/Basic/공배수.py
@@ -1,7 +1,11 @@
 def solution(number, n, m):
-    if number&n == 0 and number&m == 0:
+
+    answer = 0
+
+    if number%n == 0 and number%m == 0:
         return 1
     else:
         return 0
-    answer = 0
     return answer
+
+#%와 &는 다르다. 나머지를 구하는건 %고 &는 비트 AND 연산을 수행한다. 잘좀 보자
