a0a2441b3c2e45cb7e1911187f9c79371d21b0b9
appypaw <siment3187@gmail.com>
2024-09-25 21:37:10 +0900

    D10 Finished

--- Programmers/Basic/D10_QR code(Not Solved).py
+++ Programmers/Basic/D10_QR code(Not Solved).py
@@ -2,16 +2,18 @@
 """
 문제 설명
 
-문자열 my_string과 두 정수 m, c가 주어집니다. my_string을 한 줄에 m 글자씩 가로로 적었을 때 왼쪽부터 세로로 c번째 열에 적힌 글자들을 문자열로 return 하는 solution 함수를 작성해 주세요.
+두 정수 q, r과 문자열 code가 주어질 때, code의 각 인덱스를 q로 나누었을 때 나머지가 r인 위치의 문자를 앞에서부터 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
 제한사항
 
-    my_string은 영소문자로 이루어져 있습니다.
-    1 ≤ m ≤ my_string의 길이 ≤ 1,000
-    m은 my_string 길이의 약수로만 주어집니다.
-    1 ≤ c ≤ m
+    0 ≤ r < q ≤ 20
+    r < code의 길이 ≤ 1,000
+    code는 영소문자로만 이루어져 있습니다.
 
 """
 
-def solution(my_string, m, c):
-    answer = ''
-    return answer
+def solution(q, r, code):
+    result = []
+    for i in range(len(code)):
+        if i % q == r:
+            result.append(code[i])
+    return ''.join(result)
