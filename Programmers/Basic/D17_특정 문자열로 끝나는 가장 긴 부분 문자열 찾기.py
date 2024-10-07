702ae0e6c97672084cb9dd8ea005d006c28ef065
appypaw <siment3187@gmail.com>
2024-10-07 13:14:39 +0900

    D17 finished

---
+++ Programmers/Basic/D17_특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
@@ -0,0 +1,10 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/181872
+"""
+문제 설명
+
+문자열 myString과 pat가 주어집니다. 
+myString의 부분 문자열중 pat로 끝나는 가장 긴 부분 문자열을 찾아서 return 하는 solution 함수를 완성해 주세요.
+"""
+
+def solution(myString, pat):
+    return myString[:myString.rfind(pat) + len(pat)]
